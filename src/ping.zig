7937abc0c330ef3ce9b49fbec1a5ea180d71834b
plutov <a.pliutau@gmail.com>
2025-01-25 19:21:59 +0100

    add vaxis code
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- src/ping.zig
+++ src/ping.zig
@@ -1,56 +1,141 @@
 const std = @import("std");
+const vaxis = @import("vaxis");
+const Cell = vaxis.Cell;
+const TextInput = vaxis.widgets.TextInput;
+const border = vaxis.widgets.border;
 
-pub fn calculate(num1: f64, num2: f64, op: u8) !f64 {
-    return switch (op) {
-        '+' => num1 + num2,
-        '-' => num1 - num2,
-        '*' => num1 * num2,
-        '/' => if (num2 == 0) {
-            return error.DivisionByZero;
-        } else num1 / num2,
-        '%' => @mod(num1, num2),
-        else => error.InvalidOperation,
-    };
-}
+// This can contain internal events as well as Vaxis events.
+// Internal events can be posted into the same queue as vaxis events to allow
+// for a single event loop with exhaustive switching. Booya
+const Event = union(enum) {
+    key_press: vaxis.Key,
+    winsize: vaxis.Winsize,
+    focus_in,
+    foo: u8,
+};
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    const stdin = std.io.getStdIn().reader();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const deinit_status = gpa.deinit();
+        //fail test; can't try in defer as defer is executed after we return
+        if (deinit_status == .leak) {
+            std.log.err("memory leak", .{});
+        }
+    }
+    const alloc = gpa.allocator();
+
+    // Initialize a tty
+    var tty = try vaxis.Tty.init();
+    defer tty.deinit();
+
+    // Initialize Vaxis
+    var vx = try vaxis.init(alloc, .{});
+    // deinit takes an optional allocator. If your program is exiting, you can
+    // choose to pass a null allocator to save some exit time.
+    defer vx.deinit(alloc, tty.anyWriter());
+
+    // The event loop requires an intrusive init. We create an instance with
+    // stable pointers to Vaxis and our TTY, then init the instance. Doing so
+    // installs a signal handler for SIGWINCH on posix TTYs
+    //
+    // This event loop is thread safe. It reads the tty in a separate thread
+    var loop: vaxis.Loop(Event) = .{
+        .tty = &tty,
+        .vaxis = &vx,
+    };
+    try loop.init();
+
+    // Start the read loop. This puts the terminal in raw mode and begins
+    // reading user input
+    try loop.start();
+    defer loop.stop();
+
+    // Optionally enter the alternate screen
+    try vx.enterAltScreen(tty.anyWriter());
+
+    // We'll adjust the color index every keypress for the border
+    var color_idx: u8 = 0;
+
+    // init our text input widget. The text input widget needs an allocator to
+    // store the contents of the input
+    var text_input = TextInput.init(alloc, &vx.unicode);
+    defer text_input.deinit();
+
+    // Sends queries to terminal to detect certain features. This should always
+    // be called after entering the alt screen, if you are using the alt screen
+    try vx.queryTerminal(tty.anyWriter(), 1 * std.time.ns_per_s);
 
     while (true) {
-        try stdout.writeAll("\nCalculator (enter 'q' to quit)\n");
-        try stdout.writeAll("Enter first number: ");
+        // nextEvent blocks until an event is in the queue
+        const event = loop.nextEvent();
+        // exhaustive switching ftw. Vaxis will send events if your Event enum
+        // has the fields for those events (ie "key_press", "winsize")
+        switch (event) {
+            .key_press => |key| {
+                color_idx = switch (color_idx) {
+                    255 => 0,
+                    else => color_idx + 1,
+                };
+                if (key.matches('c', .{ .ctrl = true })) {
+                    break;
+                } else if (key.matches('l', .{ .ctrl = true })) {
+                    vx.queueRefresh();
+                } else {
+                    try text_input.update(.{ .key_press = key });
+                }
+            },
 
-        var first_input_buffer: [16]u8 = undefined;
-        const first_input = try stdin.readUntilDelimiter(&first_input_buffer, '\n');
-        if (first_input.len == 1 and first_input[0] == 'q') break;
-
-        const num1 = try std.fmt.parseFloat(f64, first_input);
-
-        try stdout.writeAll("Enter operation (+, -, *, /): ");
-
-        var op_buffer: [16]u8 = undefined;
-        const op = try stdin.readUntilDelimiter(&op_buffer, '\n');
-
-        if (op.len == 0) {
-            try stdout.writeAll("Error: No operation entered!\n");
-            continue;
+            // winsize events are sent to the application to ensure that all
+            // resizes occur in the main thread. This lets us avoid expensive
+            // locks on the screen. All applications must handle this event
+            // unless they aren't using a screen (IE only detecting features)
+            //
+            // The allocations are because we keep a copy of each cell to
+            // optimize renders. When resize is called, we allocated two slices:
+            // one for the screen, and one for our buffered screen. Each cell in
+            // the buffered screen contains an ArrayList(u8) to be able to store
+            // the grapheme for that cell. Each cell is initialized with a size
+            // of 1, which is sufficient for all of ASCII. Anything requiring
+            // more than one byte will incur an allocation on the first render
+            // after it is drawn. Thereafter, it will not allocate unless the
+            // screen is resized
+            .winsize => |ws| try vx.resize(alloc, tty.anyWriter(), ws),
+            else => {},
         }
 
-        try stdout.writeAll("Enter second number: ");
+        // vx.window() returns the root window. This window is the size of the
+        // terminal and can spawn child windows as logical areas. Child windows
+        // cannot draw outside of their bounds
+        const win = vx.window();
 
-        var second_input_buffer: [16]u8 = undefined;
-        const num2 = try std.fmt.parseFloat(f64, try stdin.readUntilDelimiter(&second_input_buffer, '\n'));
+        // Clear the entire space because we are drawing in immediate mode.
+        // vaxis double buffers the screen. This new frame will be compared to
+        // the old and only updated cells will be drawn
+        win.clear();
 
-        const result = calculate(num1, num2, op[0]) catch |err| {
-            switch (err) {
-                error.DivisionByZero => try stdout.writeAll("Error: Division by zero!\n"),
-                error.InvalidOperation => try stdout.print("Error: Invalid operation '{s}'!\n", .{op}),
-                else => return err,
-            }
-            continue;
+        // Create a style
+        const style: vaxis.Style = .{
+            .fg = .{ .index = color_idx },
         };
 
-        try stdout.print("Result: {d}\n", .{result});
+        // Create a bordered child window
+        const child = win.child(.{
+            .x_off = win.width / 2 - 20,
+            .y_off = win.height / 2 - 3,
+            .width = 40,
+            .height = 3,
+            .border = .{
+                .where = .all,
+                .style = style,
+            },
+        });
+
+        // Draw the text_input in the child window
+        text_input.draw(child);
+
+        // Render the screen. Using a buffered writer will offer much better
+        // performance, but is not required
+        try vx.render(tty.anyWriter());
     }
 }

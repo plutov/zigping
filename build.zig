567b84ced224d38c6aa9fd0a837ed98345e21c6c
plutov <a.pliutau@gmail.com>
2025-02-23 20:16:47 +0100

    better memory management
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- build.zig
+++ build.zig
@@ -2,9 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{
-        .preferred_optimize_mode = .ReleaseFast,
-    });
+    const optimize = b.standardOptimizeOption(.{});
 
     // dependencies
     const vaxis_dep = b.dependency("vaxis", .{
---
+++ src/test.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+
+// TODO: add actual tests
+test "dummy test" {
+    try std.testing.expectEqual(true, true);
+}
